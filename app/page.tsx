735228ca79214cb096601dd1770901d7ffa418f1
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2025-06-03 01:56:55 +0530

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Button } from '../components/ui/button';
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <Button>Hello</Button>
   );
 }
