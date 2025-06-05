5401b84c91856309c62c5a5d225939728ccb1e1d
SAGI VENKATA NAGA GOPAL VARMA <varmasagi101@gmail.com>
2025-06-06 02:07:16 +0530

    Hero Page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,8 +3,32 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 const HeroSection = () => {
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (imageElement && scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else if (imageElement) {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, []);
+
   return (
     <div className="pb-20 px-4">
       <div className="container mx-auto text-center">
@@ -12,19 +36,24 @@ const HeroSection = () => {
           Manage Your Finances <br /> with Intelligence
         </h1>
 
-        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+        <p className="text-xl text-gray-600 mb-4 max-w-2xl mx-auto">
           An AI-powered personal finance app that helps you track your expenses,
           set budgets, and achieve your financial goals effortlessly.
         </p>
-        <div>
+        <div className="flex justify-center items-center gap-4">
           <Link href={"/dashboard"}>
             <Button size={"lg"} className="px-8">
               Get Started
             </Button>
           </Link>
+          <Link href={"/dashboard"}>
+            <Button size={"lg"} className="px-8" variant={"outline"}>
+              Add Transaction
+            </Button>
+          </Link>
         </div>
-        <div>
-          <div>
+        <div className="hero-image-wrapper">
+          <div ref={imageRef} className="hero-image">
             <Image
               src="/banner.jpeg"
               alt="Finance dashboard illustration"
